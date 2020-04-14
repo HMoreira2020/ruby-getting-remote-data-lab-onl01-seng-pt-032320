c81bd5ac52f4ef4795b2bc1f5aeada103b556276
Heather Moreira <moreiraheather@gmail.com>
2020-04-14 00:58:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,9 +3,10 @@ require 'open-uri'
 require 'json' 
 
 class GetRequester 
+  attr_accessor :url 
   
   def initialize(string_url) 
-    
+    @url = string_url 
   end 
   
   def get_response_body 
