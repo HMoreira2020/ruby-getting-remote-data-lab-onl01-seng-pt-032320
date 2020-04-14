bd9dd20fdde9d3da5cd9934afd97dbb2105093c7
Heather Moreira <moreiraheather@gmail.com>
2020-04-14 01:03:04 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,11 +6,11 @@ class GetRequester
   # attr_accessor :url 
   
   def initialize(string_url) 
-    @url = string_url 
+    self.url = string_url 
   end 
   
   def get_response_body 
-    uri = URI.parse(@url)
+    uri = URI.parse(self.url)
     response = Net::HTTP.get_response(uri)
     response.body 
   end 
